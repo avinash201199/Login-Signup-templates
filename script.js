a6b1139444417bc728b7589a9cc7f638f283c921
Kanishkau4 <kanishkau22@gmail.com>
2025-10-09 10:34:11 +0530

    Add Kanishka sign-up and sign-in templates with dark mode support

--- script.js
+++ script.js
@@ -55,6 +55,7 @@ const templateFolders = [
   "Tech Zero",
   "Template 1",
   "Template 2",
+  "Kanishka",
 ];
 
 // A list of only the templates that were working.
@@ -96,6 +97,7 @@ const workingTemplates = [
   "Avinash",
   "Dev-Portal-Shikha",
   "Split-Screen-Dark-Shikha",
+  "Kanishka",
 ];
 
 //container element from the HTML
